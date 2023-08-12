eaf8e99de04b48b6fe01b858c12131c4e263e539
sksjw95 <tjwldnd95@hanmail.net>
2023-08-12 23:32:03 +0900

    배열을 기준으로 포인터와 합체하기

---
+++ Project22/AP3.c
@@ -0,0 +1,14 @@
+// 배열을 기준으로 포인터와 합체하기
+#include<stdio.h>
+void main() {
+	char data1, data2, data3, data4, data5;
+	char* p[5] = { &data1,&data2,&data3,&data4,&data5 };
+	for (int i = 0; i < 5; i++) {
+		*p[i] = 0;
+		printf("%d", *p[i]);
+	}
+	// char *p[5] = char *p1,*p2,*p3,*p4,*p5;
+	// *p[0] = data1, *p[1] = data2, *p[2] = data3, *p[3] = data4, *p[4] = data5;
+	// p[0] = &data1, p[1] = &data2, p[2] = &data3, p[3] = &data4, p[4] = &data5;
+
+}
