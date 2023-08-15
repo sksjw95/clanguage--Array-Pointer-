a825c2fa9bec68e07d32d4353fabb34511f73af6
sksjw95 <tjwldnd95@hanmail.net>
2023-08-15 21:06:41 +0900

    c언어 385p 나중에 다시 한번 확인

---
+++ Project22/AP6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void Show() {
+	int i, t, j;
+	printf("Show Function\n");
+}
+void Test() {
+	int y;
+	Show();
+}
+void main() {
+	int a, b, c;
+	Test();
+}
+// 16-1 프로세스와 메모리 할당 385p
